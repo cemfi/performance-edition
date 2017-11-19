83d5cd7616f701bc72e57ef05a7abf70a50af57e
Simon Waloschek <s.waloschek@cemfi.de>
2017-11-19 15:02:06 +0100

    Initial code base using vue and golden-layout

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Navigator/>
+  </div>
+</template>
+
+<script>
+  import Navigator from './components/Navigator';
+
+  export default {
+    name: 'app',
+    components: {
+      Navigator,
+    },
+  };
+</script>
+
+<style src="@/theme/goldenlayout.less" lang="less"></style>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+</style>
---
+++ src/components/Navigator.vue
@@ -0,0 +1,54 @@
+<template>
+  <layout-golden class="screen" :showPopoutIcon="false" :showCloseIcon="false" :showMaximiseIcon="false">
+    <gl-row>
+      <gl-row :width="0.8">
+        <gl-component v-for="key in activeSources" :key="key" :title="sources[key].name"></gl-component>
+      </gl-row>
+      <gl-col :width="0.2">
+        <gl-component title="Notenquellen" :height="0.3" :closable="false">
+          <div v-for="(source, key) in sources">
+            <input type="checkbox"
+                   :id="key"
+                   :name="source.name"
+                   :value="key"
+                   v-model="activeSources">
+            {{ source.name }}
+          </div>
+        </gl-component>
+        <gl-component title="Aufnahmen" :height="0.2" :closable="false"></gl-component>
+        <gl-component title="Details" :height="0.5" :closable="false"></gl-component>
+      </gl-col>
+    </gl-row>
+  </layout-golden>
+</template>
+
+<script>
+  export default {
+    name: 'Navigator',
+    data() {
+      return {
+        sources: {
+          a: {
+            name: 'Quelle A',
+          },
+          b: {
+            name: 'Quelle B',
+          },
+        },
+        activeSources: [],
+      };
+    },
+    created() {
+      Object.keys(this.sources).forEach((key) => {
+        this.activeSources.push(key);
+      });
+    },
+  };
+</script>
+
+<style scoped>
+  .screen {
+    width: 100%;
+    height: 100%;
+  }
+</style>
